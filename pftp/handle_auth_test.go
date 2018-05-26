e594eaf493ff7fc9e14ed536814ba0ae10e88826
pyama86 <pyama@pepabo.com>
2018-05-26 16:08:07 +0900

    debug

--- pftp/handle_auth_test.go
+++ pftp/handle_auth_test.go
@@ -6,6 +6,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/k0kubun/pp"
 	"github.com/pyama86/pftp/test"
 )
 
@@ -111,6 +112,8 @@ func Test_clientHandler_handleUSER(t *testing.T) {
 				context: tt.fields.context,
 			}
 			got := c.handleUSER()
+			pp.Println(got)
+			pp.Println(tt.want)
 			if (got != nil && tt.want == nil) || (tt.want != nil && (got.code != tt.want.code || got.msg != tt.want.msg)) {
 				t.Errorf("clientHandler.handleUSER() = %v, want %v", got, tt.want)
 			}
