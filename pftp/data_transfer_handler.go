8260e3d7f69bfd72e1aad007e9825d040eacf163
Jinseek Ryu <heat1024@pepabo.com>
2019-05-12 01:14:27 +0900

    send EOF to writer when receive EOF from reader(if just close, data connection did not end well everytime)

--- pftp/data_transfer_handler.go
+++ pftp/data_transfer_handler.go
@@ -1,212 +1,222 @@
 package pftp
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"net"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
 )
 
 const (
-	DATA_TRANSFER_BUFFER_SIZE = 1024
+	DATA_TRANSFER_BUFFER_SIZE = 4096
 	LISTENER_TIMEOUT          = 30
 )
 
 type dataListener struct {
-	responseCode string
-	listener     net.Listener
-	clientConn   net.Conn
-	originConn   net.Conn
-	originAddr   string
-	originIP     string
-	originPort   string
-	timeout      int
-	isConnected  bool
-	listenPort   string
-	log          *logger
+	responseCode  string
+	listener      *net.TCPListener
+	txConn        net.Conn
+	rxConn        net.Conn
+	remoteAddr    string
+	remoteIP      string
+	remotePort    int
+	keepaliveTime int
+	isConnected   bool
+	receivedIP    string
+	listenerPort  int
+	log           *logger
 }
 
 // Make listener for data connection
-func newDataListener(line string, originAddr string, timeout int, log *logger, resCode string) (string, int, error) {
-	d := &dataListener{
-		responseCode: resCode,
-		originAddr:   originAddr,
-		listener:     nil,
-		clientConn:   nil,
-		originConn:   nil,
-		timeout:      timeout,
-		isConnected:  false,
-		log:          log,
-	}
-	d.getPasvIPPort(line)
+func newDataListener(line string, receivedIP string, keepaliveTime int, log *logger, resCode string) (string, int, error) {
+	var err error
 
-	l, err := net.Listen("tcp4", d.originIP+":")
+	d := &dataListener{
+		responseCode:  resCode,
+		receivedIP:    receivedIP,
+		listener:      nil,
+		txConn:        nil,
+		rxConn:        nil,
+		keepaliveTime: keepaliveTime,
+		isConnected:   false,
+		log:           log,
+	}
+	lAddr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:")
 	if err != nil {
-		d.log.err("cannot open data channel port: %v", err)
+		d.log.err("cannot resolve TCPAddr")
 		return "", -1, err
 	}
 
-	d.listener = l
-	d.listenPort = strings.SplitN(l.Addr().String(), ":", 2)[1]
-	listenPort, _ := strconv.Atoi(d.listenPort)
+	if d.listener, err = net.ListenTCP("tcp4", lAddr); err != nil {
+		d.log.err("cannot open data channel socket: %v", err)
+		return "", -1, err
+	}
+	// set listener timeout
+	d.listener.SetDeadline(time.Now().Add(time.Duration(LISTENER_TIMEOUT) * time.Second))
 
-	// start listener & listener timer
-	go d.startDataListener()
-	go d.setListenerTimer()
+	// get listen port
+	d.listenerPort, _ = strconv.Atoi(strings.SplitN(d.listener.Addr().String(), ":", 2)[1])
 
-	return d.originIP, listenPort, nil
-}
-
-// parse PASV IP and Port from server response
-func (d *dataListener) getPasvIPPort(line string) {
-	var ip string
-	var port int
+	startIndex := strings.Index(line, "(")
+	endIndex := strings.LastIndex(line, ")")
 
 	switch d.responseCode {
 	case "PASV":
-		pasvAddr := strings.Split(strings.Trim(strings.Split(strings.Trim(line, ").\r\n"), " ")[4], "("), ",")
-		ip = ""
-		for i := 0; i < 4; i++ {
-			ip += pasvAddr[i]
-			if i < 3 {
-				ip += "."
-			}
+		// PASV response format : "227 Entering Passive Mode (h1,h2,h3,h4,p1,p2)."
+		if startIndex == -1 || endIndex == -1 {
+			err = errors.New("Invalid PASV response format")
+			return "", -1, err
 		}
-
-		port = 0
-		for i := 4; i <= 5; i++ {
-			n, _ := strconv.Atoi(pasvAddr[i])
-			if i == 4 {
-				n *= 256
-			}
-			port += n
-		}
+		d.remoteIP, d.remotePort = parseToAddr(line[startIndex+1 : endIndex])
 	case "EPSV":
-		ip = strings.Split(d.originAddr, ":")[0]
-
-		pasvPort := strings.Trim(strings.Split(strings.Trim(line, ").\r\n"), " ")[5], "()|")
-		port, _ = strconv.Atoi(pasvPort)
+		// EPSV response format : "229 Entering Extended Passive Mode (|||port|)"
+		if startIndex == -1 || endIndex == -1 {
+			err = errors.New("Invalid PASV response format")
+			return "", -1, err
+		}
+		originPort := strings.Trim(line[startIndex+1:endIndex], "|")
+		d.remoteIP = d.receivedIP
+		d.remotePort, _ = strconv.Atoi(originPort)
+	case "PORT":
+		// PORT command format : "PORT h1,h2,h3,h4,p1,p2\r\n"
+		line = strings.Split(strings.Trim(line, "\r\n"), " ")[1]
+		d.remoteIP, d.remotePort = parseToAddr(line)
 	}
 
-	d.originIP = ip
-	d.originPort = strconv.Itoa(port)
-	return
+	// start listener & listener timer
+	go d.startDataListener()
+
+	return d.receivedIP, d.listenerPort, nil
 }
 
 // Make listener for data connection
 func (d *dataListener) startDataListener() error {
-	conn, err := d.listener.Accept()
+	var err error
+
+	eg := errgroup.Group{}
+
+	defer func() {
+		// close each net.Conn absolutely(for end goroutine)
+		d.rxConn.Close()
+		d.txConn.Close()
+
+		// close listener
+		d.listener.Close()
+	}()
+
+	d.rxConn, err = d.listener.Accept()
 	if err != nil {
+		d.log.err("listen error : %s", err.Error())
 		return err
 	}
-
-	logrus.Info("Data channel connected : ", "clientIp ", conn.RemoteAddr())
-
-	// set conn to TCPConn
-	tcpConn := conn.(*net.TCPConn)
-
-	if d.timeout > 0 {
-		// set linger 0 and tcp keepalive setting between client connection
-		tcpConn.SetKeepAlive(true)
-		tcpConn.SetKeepAlivePeriod(time.Duration(d.timeout) * time.Second)
-		tcpConn.SetLinger(0)
-
-		// set deadline
-		tcpConn.SetDeadline(time.Now().Add(time.Duration(d.timeout) * time.Second))
-	}
-	d.clientConn = conn
-
-	d.isConnected = true
-
-	err = d.connectToOriginDataChan()
-	if err != nil {
-		return err
-	}
-
-	clientToOriginDone := make(chan struct{})
-	OriginToClientDone := make(chan struct{})
-
-	// client to origin
-	go d.dataTransfer(d.clientConn, d.originConn, clientToOriginDone)
-
-	// origin to client
-	go d.dataTransfer(d.originConn, d.clientConn, OriginToClientDone)
-
-	// wait until data transfer is completely done
-	<-clientToOriginDone
-	<-OriginToClientDone
-
-	// close listener
+	// close listener immediatly
 	d.listener.Close()
 	d.listener = nil
 
-	d.log.debug("Data channel disconnected")
+	d.log.info("Data channel connected from %s", d.rxConn.RemoteAddr().String())
+
+	// set conn to TCPConn
+	tcpConn := d.rxConn.(*net.TCPConn)
+
+	if d.keepaliveTime > 0 {
+		// set linger 0 and tcp keepalive setting between client connection
+		tcpConn.SetKeepAlive(true)
+		tcpConn.SetKeepAlivePeriod(time.Duration(d.keepaliveTime) * time.Second)
+		tcpConn.SetLinger(0)
+
+		// set deadline
+		tcpConn.SetDeadline(time.Now().Add(time.Duration(d.keepaliveTime) * time.Second))
+	}
+
+	err = d.connectToRemoteDataChan()
+	if err != nil {
+		return err
+	}
+	// txConn to rxConn
+	eg.Go(func() error { return d.dataTransfer(d.rxConn, d.txConn) })
+
+	// rxConn to txConn
+	eg.Go(func() error { return d.dataTransfer(d.txConn, d.rxConn) })
+
+	// wait until data transfer goroutine end
+	if err = eg.Wait(); err != nil {
+		d.log.err(err.Error())
+	}
+
+	d.log.debug("proxy data channel disconnected")
 
 	return nil
 }
 
 // Connect to origin server data channel
-func (d *dataListener) connectToOriginDataChan() error {
-	conn, err := net.Dial("tcp", d.originIP+":"+d.originPort)
+func (d *dataListener) connectToRemoteDataChan() error {
+	var err error
+
+	d.txConn, err = net.Dial("tcp", net.JoinHostPort(d.remoteIP, strconv.Itoa(d.remotePort)))
 	if err != nil {
 		return err
 	}
 
 	// set conn to TCPConn
-	tcpConn := conn.(*net.TCPConn)
+	tcpConn := d.txConn.(*net.TCPConn)
 
-	// set linger 0 to local origin ftp server
 	// set linger 0 and tcp keepalive setting between client connection
 	tcpConn.SetKeepAlive(true)
-	tcpConn.SetKeepAlivePeriod(time.Duration(d.timeout) * time.Second)
+	tcpConn.SetKeepAlivePeriod(time.Duration(d.keepaliveTime) * time.Second)
 	tcpConn.SetLinger(0)
 
 	// set deadline
-	tcpConn.SetDeadline(time.Now().Add(time.Duration(d.timeout) * time.Second))
-
-	d.originConn = conn
+	tcpConn.SetDeadline(time.Now().Add(time.Duration(d.keepaliveTime) * time.Second))
 
 	return nil
 }
 
-// check data channel connected and close listener if listening over 30 seconds
-func (d *dataListener) setListenerTimer() {
-	for i := 0; i < LISTENER_TIMEOUT; i++ {
-		if d.isConnected {
-			return
-		}
-		time.Sleep(time.Duration(1) * time.Second)
+// send data until got EOF
+func (d *dataListener) dataTransfer(reader net.Conn, writer net.Conn) error {
+	var err error
+
+	buffer := make([]byte, DATA_TRANSFER_BUFFER_SIZE)
+	if bytes, err := io.CopyBuffer(writer, reader, buffer); err == nil {
+		d.log.debug("data transfer in porxy successs with %d byte", bytes)
+		err = nil
+	} else {
+		err = fmt.Errorf("got error on data transfer: %s", err.Error())
 	}
 
-	if !d.isConnected {
-		if d.listener != nil {
-			d.listener.Close()
-			d.listener = nil
-			d.log.debug("Data channel disconnected by timeout: %s", d.listener.Addr().String())
-		}
-	}
+	// send EOF to writer
+	shutdownWrite(writer)
 
-	return
+	return err
 }
 
-// send data until got EOF
-func (d *dataListener) dataTransfer(reader net.Conn, writer net.Conn, done chan struct{}) {
-	buffer := make([]byte, DATA_TRANSFER_BUFFER_SIZE)
+// parse PASV IP and Port from server response
+func parseToAddr(line string) (string, int) {
+	var ip string = ""
 
-	for {
-		if _, err := reader.Read(buffer); err != nil {
-			break
-		}
-		if _, err := writer.Write(buffer); err != nil {
-			break
+	addr := strings.Split(line, ",")
+	for i := 0; i < 4; i++ {
+		ip += addr[i]
+		if i < 3 {
+			ip += "."
 		}
 	}
-	reader.Close()
-	writer.Close()
 
-	done <- struct{}{}
+	// Let's compute the port number
+	port1, _ := strconv.Atoi(addr[4])
+	port2, _ := strconv.Atoi(addr[5])
 
-	return
+	return ip, (port1 * 256) + port2
+}
+
+// send EOF to write
+func shutdownWrite(conn net.Conn) {
+	// anonymous interface. Could explicitly use TCP instead.
+	if v, ok := conn.(interface{ CloseWrite() error }); ok {
+		v.CloseWrite()
+	}
 }
