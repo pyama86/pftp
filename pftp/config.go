13a858f62b16662e5dc1f19933fceafb78c22e41
Jinseek Ryu <heat1024@pepabo.com>
2019-05-27 14:25:35 +0900

    set const param to port range length

--- pftp/config.go
+++ pftp/config.go
@@ -12,6 +12,12 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+const (
+	// PORT_RANGE_LENGTH is const parameter for port range configuration check
+	// port range must set like 100-110 so it might split 2 strings by '-'
+	PORT_RANGE_LENGTH = 2
+)
+
 type portRange struct {
 	Start int
 	End   int
@@ -138,7 +144,7 @@ func dataPortRangeValidation(r string) error {
 	lastErr := fmt.Errorf("Data port range config wrong. set default(random port)")
 	portRange := strings.Split(r, "-")
 
-	if len(portRange) != 2 {
+	if len(portRange) != PORT_RANGE_LENGTH {
 		return lastErr
 	}
 
